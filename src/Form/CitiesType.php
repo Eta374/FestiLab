199f902fc0bd9e04b9b4b3b4b92c60b2a4143bda
Erwan Gentric <infogentric@gmail.com>
2021-09-21 16:14:45 +0200

    CRUD cities Departement

---
+++ src/Form/CitiesType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Cities;
+use App\Entity\Departments;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CitiesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nameUppercase')
+            ->add('nameSimple')
+            ->add('nameReal')
+            ->add('zip')
+            ->add('longitudeDeg')
+            ->add('latitudeDeg')
+            // ->add('department')
+            ->add('department', ChoiceType::class, [
+                'class' => Departments::class,
+                'multiple' => true,
+                'expanded' => true,
+                'choice_label' => 'name',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Cities::class,
+        ]);
+    }
+}
