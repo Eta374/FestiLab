c42e70a154fde1a8ea095bfba144e3109cd6799d
Erwan Gentric <infogentric@gmail.com>
2021-09-23 14:52:04 +0200

    CSS form contact

--- src/Form/NewsType.php
+++ src/Form/NewsType.php
@@ -4,9 +4,10 @@ namespace App\Form;
 
 use App\Entity\News;
 use Symfony\Component\Form\AbstractType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
 use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 
 class NewsType extends AbstractType
